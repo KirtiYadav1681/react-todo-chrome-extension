3f3638ffd4be3451eaff66f706801fb7df0a742b
KirtiYadav1681 <yadavkirti7745@gmail.com>
2024-06-14 18:29:53 +0530

    modified redux implementation

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,48 +1,6 @@
 import {createStore} from "redux";
+import todoReducer from "./reducer";
 
-const initialState = {todos: []}
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "NEW_TODO":
-            {
-                const newTodo = {key: state.todos.length + 1, todo: action.payload, isComplete:false}
-                return {...state, todos: [...state.todos, newTodo]}
-            }
-        case "COMPLETE_TODO":
-            {
-                const todos = state.todos.map((todo) =>
-                    todo.key === action.payload
-                      ? { ...todo, isComplete: !todo.isComplete }
-                      : todo
-                  );
-                  return { ...state, todos };
-            }
-        case "EDIT_TODO":
-            {
-                const todos = state.todos.map((todo) =>
-                    todo.key === action.payload.key
-                      ? { ...todo, todo: action.payload.todo }
-                      : todo
-                  );
-                  return { ...state, todos };
-            }
-        case "DELETE_TODO":
-            {
-                const todos = state.todos.filter((todo) =>  todo.key !== action.payload);
-                return {...state, todos};
-            }
-        case "DELETE_ALL": return {...state, todos:[]};
-        case "DELETE_COMPLETED_TODOS":
-            {
-                const todos = state.todos.filter((todo) => todo.isComplete !== true);
-                return {...state, todos};
-            }
-        default:
-            return state;
-    }
-}
-
-const store = createStore(reducer);
+const store = createStore(todoReducer);
 
 export default store;
